8c0aa3048516bd67002bd7b8565bfc7e66311fb8
Fuad Maulana <fuadm03@gmail.com>
2023-11-14 09:08:06 +0700

    feat: register account

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'success register',
+            'access_token' => $user->createToken('api_token')->plainTextToken,
+            'token_type' => 'Bearer'
+        ], 201);
+    }
+
+    public function login()
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+}
