7a0401dde080801864307365d43a0ccc4a40225a
Fuad Maulana <fuadm03@gmail.com>
2023-11-14 10:10:47 +0700

    feat: User Login

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterUserRequest;
+use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
-use App\Http\Requests\RegisterUserRequest;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -22,12 +25,25 @@ class AuthController extends Controller
             'message' => 'success register',
             'access_token' => $user->createToken('api_token')->plainTextToken,
             'token_type' => 'Bearer'
-        ], 201);
+        ], Response::HTTP_CREATED);
     }
 
-    public function login()
+    public function login(LoginRequest $request)
     {
+        $user = User::where('email', $request->validated()['email'])->first();
 
+        if (! $user || ! Hash::check($request->validated()['password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $expiresAt = $request->remember ? null : now()->addMinutes(config('session.lifetime'));
+
+        return response()->json([
+            'access_token' => $user->createToken('api_token', expiresAt: $expiresAt)->plainTextToken,
+            'token_type' => 'Bearer'
+        ], Response::HTTP_CREATED);
     }
 
     public function logout()
--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('auth')->group(function() {
     Route::post('/register', [V1\AuthController::class, 'register']);
+    Route::post('/login', [V1\AuthController::class, 'login']);
 });
