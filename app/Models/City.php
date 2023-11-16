021692f9b72086d7aa045c2d7594a3c59cb63fb4
Fuad Maulana <fuadm03@gmail.com>
2023-11-16 09:25:39 +0700

    fix: HasMany import

--- app/Models/City.php
+++ app/Models/City.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\Province;
 use App\Models\Subdistrict;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
