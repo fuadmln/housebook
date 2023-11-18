a9c9b2a1f92160be23796ff309f8c24d24d5be02
Fuad Maulana <fuadm03@gmail.com>
2023-11-19 00:01:49 +0700

    enums define PropertyType and Booking Status

---
+++ app/Enums/BookingStatus.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Enums;
+
+enum BookingStatus: int
+{
+    case PENDING = 0;
+    case ACCEPTED = 1;
+    case REJECTED = 2;
+    case DONE = 3;
+
+    public static function getString($val): string
+    {
+        return match ($val) {
+            self::PENDING => 'pending',
+            self::ACCEPTED => 'accepted',
+            self::REJECTED => 'rejected',
+            self::DONE => 'done',
+        };
+    }
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+    public static function toArray(): array
+    {
+        $cases = [];
+
+        foreach (self::cases() as $case) {
+            $cases[$case->name] = $case->value;
+        }
+
+        return $cases;
+    }
+}
--- app/Enums/PropertyType.php
+++ app/Enums/PropertyType.php
@@ -4,16 +4,30 @@ namespace App\Enums;
 
 enum PropertyType: int
 {
-    // use Enums;
-
     case NEW = 0;
     case SECOND = 1;
 
     public static function getString($val): string
     {
         return match ($val) {
-            self::NEW => 'New',
-            self::SECOND => 'Second',
+            self::NEW => 'new',
+            self::SECOND => 'second',
         };
+    }
+
+    public static function toArray(): array
+    {
+        $cases = [];
+
+        foreach (self::cases() as $case) {
+            $cases[$case->name] = $case->value;
+        }
+
+        return $cases;
+    }
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
     }
 }
