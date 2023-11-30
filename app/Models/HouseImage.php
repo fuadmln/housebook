002177a8e014ae0917b8670ca99b2e367c30239c
Fuad Maulana <fuadm03@gmail.com>
2023-11-30 14:37:36 +0700

    feat: cast house image url with app url

--- app/Models/HouseImage.php
+++ app/Models/HouseImage.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\House;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -14,6 +15,13 @@ class HouseImage extends Model
     protected $fillable = ['house_id', 'url', 'sequence', 'file_path'];
 
     protected $hidden = ['created_at', 'updated_at'];
+
+    protected function url(): Attribute
+    {
+        return new Attribute(
+            get: fn () => asset('storage/' . $this->file_path)
+        );
+    }
 
     public function house(): BelongsTo
     {
